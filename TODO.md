5394e2a2db6ec9cbe6a729d091b7afb2dcd5cea0
CopperSlab07 <johnanderson4096@gmail.com>
2026-06-08 23:04:49 +0700

    Temporarily implemented states

---
+++ TODO.md
@@ -0,0 +1,3 @@
+# 8/6/2026
+## Cube:
+* [x] Introducing "states" (gameplay, pause for now)
--- cube.c
+++ cube.c
@@ -9,7 +9,8 @@
 #define CUBE_COLOR_OUTLINE_DEFAULT RAYWHITE
 
 Vector3 cube_pos[MAX_CUBES] = {0};
-int cube_count = 1;
+int cube_count = 0;
+
 void cube_append(void){
     if(cube_count < MAX_CUBES){
         int index = cube_count;
@@ -20,8 +21,9 @@ void cube_append(void){
         TraceLog(LOG_INFO, "You fat fuck, there's no more space!");
     }
 }
+
 void cube_pop(void){
-    if(cube_count > 1){
+    if(cube_count > 0){
         cube_pos[cube_count] = (Vector3){.x = 0, .y = 0, .z = 0};
         cube_count--;
     }
@@ -30,7 +32,16 @@ void cube_pop(void){
     }
 }
 
+typedef enum States {
+    STATE_DEFAULT = 0,
+    STATE_PAUSED = 1
+} States;
+
 int main(void){
+    int state = STATE_DEFAULT, prev_state = STATE_DEFAULT;
+    Image screenshot = {0};
+    Texture2D paused_background = {0};
+
     Camera3D cam = {
         .position = (Vector3){.x = 1, .y = 1, .z = 1},
         .target = (Vector3){.x = 0, .y = 0, .z = 0},
@@ -43,51 +54,52 @@ int main(void){
     SetTargetFPS(60);
     SetExitKey(KEY_Q);
     DisableCursor();
-    int paused = 0;
-    Image screenshot = {0};
-    Texture2D temp = {0};
     while(!WindowShouldClose()){
-        if(IsKeyPressed(KEY_R)){
-            //spawn = 1;
-            cube_append();
+        //State handling
+        if(IsKeyPressed(KEY_ESCAPE)){
+            screenshot = LoadImageFromScreen();
+            paused_background = LoadTextureFromImage(screenshot);
+            if(state != STATE_PAUSED){
+                prev_state = state;
+                state = STATE_PAUSED;
+            } else {
+                state = prev_state;
+                prev_state = STATE_PAUSED;
+            }
         }
 
-        if(IsKeyPressed(KEY_E)){
-            cube_pop();
-        }
-        
-        if(!paused) UpdateCamera(&cam, CAMERA_FREE);
+        //Actual drawing
         BeginDrawing();
 
-        if (!paused){
+        switch(state){
+        case STATE_DEFAULT: {
+            if(IsKeyPressed(KEY_R)){
+                //spawn = 1;
+                cube_append();
+            }
+            
+            if(IsKeyPressed(KEY_E)){
+                cube_pop();
+            }
+        
+            UpdateCamera(&cam, CAMERA_FREE);            
             ClearBackground(GetColor(0x181818FF));
             BeginMode3D(cam);
-            /*
-                DrawCube((Vector3){0, 0, 0}, 1, 1, 1, GREEN);
-                DrawCubeWires((Vector3){0, 0, 0}, 1, 1, 1, RAYWHITE);
-            */
             for(int i = 0; i < cube_count; i++){
                 DrawCube(cube_pos[i], CUBE_SIZE, CUBE_SIZE, CUBE_SIZE, CUBE_COLOR_DEFAULT);
                 DrawCubeWires(cube_pos[i], CUBE_SIZE, CUBE_SIZE, CUBE_SIZE, CUBE_COLOR_OUTLINE_DEFAULT);
-            }
-            
+            }            
             EndMode3D();
         }
- 
-        if(IsKeyPressed(KEY_ESCAPE)){
-            screenshot = LoadImageFromScreen();
-            temp = LoadTextureFromImage(screenshot);
-            paused = !paused;
-        }
-
-        if(paused){
-            DrawTexture(temp, 0, 0, GetColor(0xFFFFFFFF));
+            break;
+        case STATE_PAUSED: {
+            DrawTexture(paused_background, 0, 0, GetColor(0xFFFFFFFF));
             DrawRectangle(0, 0, DISPLAY_WIDTH, DISPLAY_HEIGHT, GetColor(0x08080888));
             DrawText("PAUSED", (DISPLAY_WIDTH - MeasureText("PAUSED", PAUSED_BANNER_SIZE)) / 2, (DISPLAY_HEIGHT - PAUSED_BANNER_SIZE) / 2, PAUSED_BANNER_SIZE, GetColor(0xFFFFFFFF));
         }
-
+    }
         DrawFPS(DISPLAY_WIDTH - 80, DISPLAY_HEIGHT - 20);
-        EndDrawing();                
+        EndDrawing();
     }
     CloseWindow();
     return 0;
