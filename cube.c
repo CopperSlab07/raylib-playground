c635e6dc2870dbe4d4f7a3161d38c39acb65cde6
CopperSlab07 <johnanderson4096@gmail.com>
2026-06-09 14:01:36 +0700

    changed UpdateCamera() mode to CAMERA_FIRST_PERSON

--- cube.c
+++ cube.c
@@ -1,3 +1,4 @@
+#include <stdint.h>
 #include <raylib.h>
 
 #define DISPLAY_WIDTH  800
@@ -8,14 +9,34 @@
 #define CUBE_COLOR_DEFAULT GREEN
 #define CUBE_COLOR_OUTLINE_DEFAULT RAYWHITE
 
+typedef struct Platform {
+    Vector3 origin;
+    Vector3 size;
+    Color color;
+} Platform;
+
+typedef struct Platforms {
+    Platform platforms[MAX_CUBES];
+    size_t count;
+} Platforms;
+
+Platform base = {
+    .origin = (Vector3){-50, -50, 0},
+    .size = (Vector3){100, 100, -1},
+};
+
 Vector3 cube_pos[MAX_CUBES] = {0};
 int cube_count = 0;
 
 void cube_append(void){
-    if(cube_count < MAX_CUBES){
-        int index = cube_count;
+    if(cube_count == 0){
+        cube_pos[cube_count] = (Vector3){0, 0, 0};
         cube_count++;
+    }
+    else if(cube_count < MAX_CUBES){
+        int index = cube_count - 1;
         cube_pos[cube_count] = (Vector3){.x = cube_pos[index].x + 1, .y = cube_pos[index].y, .z = cube_pos[index].z};
+        cube_count++;
     }
     else {
         TraceLog(LOG_INFO, "You fat fuck, there's no more space!");
@@ -38,6 +59,7 @@ typedef enum States {
 } States;
 
 int main(void){
+    base.color = GetColor(0x18FFAAFF);
     int state = STATE_DEFAULT, prev_state = STATE_DEFAULT;
     Image screenshot = {0};
     Texture2D paused_background = {0};
@@ -60,6 +82,7 @@ int main(void){
             if(state != STATE_PAUSED){
                 screenshot = LoadImageFromScreen();
                 paused_background = LoadTextureFromImage(screenshot);
+                SetWindowIcon(screenshot);
                 prev_state = state;
                 state = STATE_PAUSED;
             } else {
@@ -67,37 +90,48 @@ int main(void){
                 prev_state = STATE_PAUSED;
             }
         }
-
+        
+        //Input handling (after state handling)
+        switch(state){
+            case STATE_DEFAULT: {
+                if(IsKeyPressed(KEY_R)){
+                    //spawn = 1;
+                    cube_append();
+                }
+            
+                if(IsKeyPressed(KEY_E)){
+                    cube_pop();
+                }
+        
+                UpdateCamera(&cam, CAMERA_FIRST_PERSON);
+            }
+            break;
+        }
+        
         //Actual drawing
         BeginDrawing();
 
         switch(state){
-        case STATE_DEFAULT: {
-            if(IsKeyPressed(KEY_R)){
-                //spawn = 1;
-                cube_append();
+            case STATE_DEFAULT: {
+                ClearBackground(GetColor(0x181818FF));
+                BeginMode3D(cam);
+                DrawCube(base.origin, base.size.x, base.size.y, base.size.z, base.color);
+                DrawCubeWires(base.origin, base.size.x, base.size.y, base.size.z, RAYWHITE);
+                for(int i = 0; i < cube_count; i++){
+                    DrawCube(cube_pos[i], CUBE_SIZE, CUBE_SIZE, CUBE_SIZE, CUBE_COLOR_DEFAULT);
+                    DrawCubeWires(cube_pos[i], CUBE_SIZE, CUBE_SIZE, CUBE_SIZE, CUBE_COLOR_OUTLINE_DEFAULT);
+                }
+                EndMode3D();
             }
-            
-            if(IsKeyPressed(KEY_E)){
-                cube_pop();
-            }
-        
-            UpdateCamera(&cam, CAMERA_FREE);            
-            ClearBackground(GetColor(0x181818FF));
-            BeginMode3D(cam);
-            for(int i = 0; i < cube_count; i++){
-                DrawCube(cube_pos[i], CUBE_SIZE, CUBE_SIZE, CUBE_SIZE, CUBE_COLOR_DEFAULT);
-                DrawCubeWires(cube_pos[i], CUBE_SIZE, CUBE_SIZE, CUBE_SIZE, CUBE_COLOR_OUTLINE_DEFAULT);
-            }            
-            EndMode3D();
-        }
             break;
-        case STATE_PAUSED: {
-            DrawTexture(paused_background, 0, 0, GetColor(0xFFFFFFFF));
-            DrawRectangle(0, 0, DISPLAY_WIDTH, DISPLAY_HEIGHT, GetColor(0x08080888));
-            DrawText("PAUSED", (DISPLAY_WIDTH - MeasureText("PAUSED", PAUSED_BANNER_SIZE)) / 2, (DISPLAY_HEIGHT - PAUSED_BANNER_SIZE) / 2, PAUSED_BANNER_SIZE, GetColor(0xFFFFFFFF));
+            
+            case STATE_PAUSED: {
+                DrawTexture(paused_background, 0, 0, GetColor(0xFFFFFFFF));
+                DrawRectangle(0, 0, DISPLAY_WIDTH, DISPLAY_HEIGHT, GetColor(0x08080888));
+                DrawText("PAUSED", (DISPLAY_WIDTH - MeasureText("PAUSED", PAUSED_BANNER_SIZE)) / 2, (DISPLAY_HEIGHT - PAUSED_BANNER_SIZE) / 2, PAUSED_BANNER_SIZE, GetColor(0xFFFFFFFF));
+            }
         }
-    }
+
         DrawFPS(DISPLAY_WIDTH - 80, DISPLAY_HEIGHT - 20);
         EndDrawing();
     }
